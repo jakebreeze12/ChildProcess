f2cc551af367eb138c68865587dd331ae5440a8f
Jake Brzeczek <jbrzeczek99@gmail.com>
2024-10-14 11:19:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Child process started.");
+
+            foreach (var arg in args)
+            { 
+                Console.WriteLine($"Argument: {arg}");
+            }
+
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("Child process completed.");
+        }
+    }
+}
